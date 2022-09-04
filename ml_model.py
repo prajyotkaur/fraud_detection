fe8c62054d8bcb0fc3cd81bbf9068de68abc409f
prajyotkaur <prajyotkaur@gmail.com>
2022-09-04 12:27:33 +0530

    Add ML Models code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,9 @@
 print("Adding all ML related code here")
+
+def model1():
+    print("Adding code for ML model 1")
+def model2():
+    print("Adding code for ML model 2")
+
+def model3():
+    print("Adding code for ML model 3")
